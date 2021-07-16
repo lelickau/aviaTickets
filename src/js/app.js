05bd1bdf05d854d3e502b2dcec54147a5d868c8d
elena_kau <lelickau@yandex.ru>
2021-07-16 16:54:53 +0200

    Создание UI для формы

---
+++ src/js/app.js
@@ -0,0 +1,9 @@
+import '../css/style.css';
+import './plugins';
+import locations from "./store/location";
+
+locations.init().then(res => {
+    console.log(res)
+    console.log(locations)
+    console.log(locations.getCitiesByCountryCode('PF'))
+})
